c1a7172805a63635c1f90f54a23aee6df5e75b6c
ALCOHOLIC <87254338+Usermax77@users.noreply.github.com>
2022-01-08 17:41:20 +0530

    Update Alcoholic.py
    Saved

--- Alcoholic.py
+++ Alcoholic.py
@@ -21,7 +21,7 @@ from telethon.tl.functions.messages import GetFullChatRequest
 from Config import STRING, SUDO, BIO_MESSAGE, ALIVE_NAME, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, STRING11, STRING12 , STRING13 , STRING14 , STRING15 ,STRING16 , STRING17 , STRING18 , STRING19 , STRING20 , STRING21 , STRING22 , STRING23 , STRING24 , STRING25 , STRING26 , STRING27 , STRING28 , STRING29 , STRING30
 import asyncio
 import telethon.utils
-from telethon.import.functions
+from telethon.tl import functions
 from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from Utils import RAID, RRAID
